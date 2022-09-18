50e54e52dcd197bd92aa6f084168c301821f2521
RuchaRK <ruchakathar@gmail.com>
2022-09-18 10:31:55 +0530

    get the inputs date and number

---
+++ app.js
@@ -0,0 +1,9 @@
+var birthDate = document.querySelector("#birth-date")
+var luckyNo = document.querySelector("#lucky-number")
+var checkButton = document.querySelector("#chck-btn")
+
+function validateNumber()
+{
+    console.log("hi")
+}
+checkButton.addEventListener("click",validateNumber)
