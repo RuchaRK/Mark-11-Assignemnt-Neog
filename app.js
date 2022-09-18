9808d0159a894676f2583e2d8baa44a19b67d48b
RuchaRK <ruchakathar@gmail.com>
2022-09-18 11:38:37 +0530

    Check if birthday is lucky

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var birthDate = document.querySelector("#birth-date")
 var luckyNo = document.querySelector("#lucky-number")
 var checkButton = document.querySelector("#chck-btn")
+var output = document.querySelector("#message")
 
 
 function calculateSum(date){
@@ -9,14 +10,26 @@ function calculateSum(date){
     for (var element of newDate) {
         element = Number(element)
         sum = sum+element
-     
       }
       return sum
+}
+function checkLucky(sum,number)
+{
+    if(sum%number === 0)
+    {
+        output.innerHTML = "Youe Birth date is lucky"
+
+    }else{
+    output.innerHTML = "Youe Birth date is not lucky"
+    }
+
 }
 
 function validateNumber()
 {  
     var date = birthDate.value
+    var number = luckyNo.value
     var sum = calculateSum(date)
+    checkLucky(sum,number)
 }
 checkButton.addEventListener("click",validateNumber)
