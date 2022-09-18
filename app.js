635d9868ce8267c5487e29ec4b93c4e96e5163cf
RuchaRK <ruchakathar@gmail.com>
2022-09-18 11:16:06 +0530

    calculate sum

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var birthDate = document.querySelector("#birth-date")
 var luckyNo = document.querySelector("#lucky-number")
 var checkButton = document.querySelector("#chck-btn")
 
+
+function calculateSum(date){
+    let sum=0
+    var newDate = date.replaceAll("-","") 
+    for (var element of newDate) {
+        element = Number(element)
+        sum = sum+element
+     
+      }
+      return sum
+}
+
 function validateNumber()
-{
-    console.log("hi")
+{  
+    var date = birthDate.value
+    var sum = calculateSum(date)
 }
 checkButton.addEventListener("click",validateNumber)
